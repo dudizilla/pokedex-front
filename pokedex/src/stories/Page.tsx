2e5e5ec71290e9410dcd4361eae9dfac72b69468
Eduarda Cunha <maria.cunha@poatek.com>
2024-07-16 18:14:19 -0300

    chore: Remove unused code in Page component

--- pokedex/src/stories/Page.tsx
+++ pokedex/src/stories/Page.tsx
@@ -35,10 +35,6 @@ export const Page: React.FC = () => {
         </p>
         <ul>
           <li>
-            Use a higher-level connected component. Storybook helps you compose such data from the
-            "args" of child component stories
-          </li>
-          <li>
             Assemble data in the page component from your services. You can mock these services out
             using Storybook.
           </li>
