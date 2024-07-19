0e2e365a3b5ea8e218209bdde1533b92bab1ee0a
Eduarda Cunha <maria.cunha@poatek.com>
2024-07-19 15:59:34 -0300

    feat: Add values prop to FilterButton component

--- pokedex/src/stories/FilterButton/FilterButton.stories.ts
+++ pokedex/src/stories/FilterButton/FilterButton.stories.ts
@@ -18,17 +18,20 @@ type Story = StoryObj<typeof meta>;
 export const Small: Story = {
   args: {
     size: 'small',
+    values: [],
   },
 };
 
 export const Medium: Story = {
   args: {
     size: 'medium',
+    values: [],
   },
 };
 
 export const Large: Story = {
   args: {
     size: 'large',
+    values: [],
   },
 };
